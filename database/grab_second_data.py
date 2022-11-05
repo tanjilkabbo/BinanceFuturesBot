45ecea66d0562979e47b780e73b7a33e1273b2d4
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-11-05 19:54:47 +0600

    Update: grab_second_data.py " Time Experiment"

--- database/grab_second_data.py
+++ database/grab_second_data.py
@@ -2,6 +2,11 @@
 Get CSV Data : https://www.cryptodatadownload.com/data/binance/
 """
 import sqlite3
+from datetime import datetime
+
+import pandas as pd
+
+from pandas import Timestamp
 
 from dataframe.dataframe import GetDataframe
 
@@ -15,27 +20,53 @@ time_of_data = int(minute)
 
 
 data = GetDataframe().get_minute_data("BTCBUSD", 1, time_of_data)
-# print(data["Open"])
-open_data = data["Open"]
-for data in open_data:
-    print(data)
+print(data)
+# print(data.iloc[1])
+# print(data.iloc[1][-1])
+# print(data.index[1])
+# open_data = data["Open"]
+for i in range(len(data)):
+    # print(single_data)
+    open_position = data['Open'].iloc[i]
+    high_position = data['High'].iloc[i]
+    low_position = data['Low'].iloc[i]
+    close_position = data['Close'].iloc[i]
+    volume_position = data['Volume'].iloc[i]
+    change_position = data['Change'].iloc[i]
+    symbol_position = data['symbol'].iloc[i]
+
+    # time_position = data['Time'].iloc[i]
+    time_position = data.index[i]
+
+    # time_position = '2019-01-15 13:30:00'
+
+    print(time_position)
+
+    unix_time = time_position.timestamp()
+    # unix_time = pd.to_datetime(time_position).astype(int) / 10**9
+    print(unix_time)
+    print(datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S'))
+    print(datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S'))
+    print(input("Stop :"))
+
+    print(f"{open_position}, {high_position}, {low_position}, {close_position}, {volume_position}, {change_position}, {symbol_position} , {time_position}")
 
     connection = sqlite3.connect("../database/cripto.db")
     cur = connection.cursor()
 
-    print(cur)
+    # print(cur)
 
     cur.execute(
         "INSERT INTO btcbusd VALUES (:id, :symbol, :Open, :High, :Low,  :Close,:Volume, :Change , :Time)",
         {
             'id': None,
-            'symbol': "ETHBUSD",
-            'Open': data,
-            'High': None,
-            'Low': None,
-            'Close': None,
-            'Volume': None,
-            'Change': None,
+            'symbol': symbol_position,
+            'Open': open_position,
+            'High': high_position,
+            'Low': low_position,
+            'Close': close_position,
+            'Volume': volume_position,
+            'Change': change_position,
             'Time': None
         })
 
