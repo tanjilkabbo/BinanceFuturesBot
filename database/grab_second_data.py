92968700d6de784b69c7246ffa35f0dc984eb95f
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-11-05 20:08:45 +0600

    Update: grab_second_data.py "Create database"

--- database/grab_second_data.py
+++ database/grab_second_data.py
@@ -21,10 +21,6 @@ time_of_data = int(minute)
 
 data = GetDataframe().get_minute_data("BTCBUSD", 1, time_of_data)
 print(data)
-# print(data.iloc[1])
-# print(data.iloc[1][-1])
-# print(data.index[1])
-# open_data = data["Open"]
 for i in range(len(data)):
     # print(single_data)
     open_position = data['Open'].iloc[i]
@@ -34,22 +30,9 @@ for i in range(len(data)):
     volume_position = data['Volume'].iloc[i]
     change_position = data['Change'].iloc[i]
     symbol_position = data['symbol'].iloc[i]
-
-    # time_position = data['Time'].iloc[i]
     time_position = data.index[i]
-
-    # time_position = '2019-01-15 13:30:00'
-
-    print(time_position)
-
     unix_time = time_position.timestamp()
-    # unix_time = pd.to_datetime(time_position).astype(int) / 10**9
-    print(unix_time)
-    print(datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S'))
-    print(datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S'))
-    print(input("Stop :"))
-
-    print(f"{open_position}, {high_position}, {low_position}, {close_position}, {volume_position}, {change_position}, {symbol_position} , {time_position}")
+    print(f"{open_position}, {high_position}, {low_position}, {close_position}, {volume_position}, {change_position}, {symbol_position} , {time_position}, {unix_time}")
 
     connection = sqlite3.connect("../database/cripto.db")
     cur = connection.cursor()
@@ -67,7 +50,7 @@ for i in range(len(data)):
             'Close': close_position,
             'Volume': volume_position,
             'Change': change_position,
-            'Time': None
+            'Time': unix_time
         })
 
     connection.commit()
